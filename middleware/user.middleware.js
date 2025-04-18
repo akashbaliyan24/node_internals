44ae94d43f6124fc740d735a0cc49db2944eaf14
Akash <akashbaliya24@gmail.com>
2025-04-18 17:07:28 +0530

    some chnages in file

---
+++ middleware/user.middleware.js
@@ -0,0 +1,24 @@
+import jwt, { decode } from "jsonwebtoken"
+import dotenv from "dotenv"
+import { User } from "../models/user.model.js"
+
+dotenv.config();
+
+const authMiddleware = async(req,resizeBy,next)=>{
+    try {
+        const token = req.cookies.token;
+
+        if(!token){
+            return resizeBy.status(400).json({message : "Unauthorized : No token Found"})
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECERET)
+
+        req.user = await User.findById(decoded.id).select("-password");
+        next(); // move to the next route / controller
+    } catch(err){
+        return resizeBy.status(400).json({message :"Unauthorized :invalid token "})
+    }
+}
+
+export default authMiddleware;
