d1b4f78e2270641954861c296f9037a1bfb28593
Akash <akashbaliya24@gmail.com>
2025-04-18 16:50:53 +0530

    there are some changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -81,3 +81,141 @@ const verifyUser = async (req, res) => {
         return res.status(400).json({ message: "something went wrong during verification " })
     }
 };
+
+const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({ message: "plesase fill the required fields" });
+    }
+    try {
+        const user = await User.findOne({ email })
+        if (!user) {
+            res.status(400).json({ message: "invalid crrdentials" })
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password)
+
+        if (!isPasswordMatch) {
+            res.status(400).json({ message: "Invalid Password" });
+        }
+        if (!user.isVerified) {
+            res.status(400).json({ message: "please verify your account first" });
+        }
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+            expiresIn: process.env.JWT_EXPIRES_IN,
+        });
+        // set token in cookie 
+        const cookieOptions = {
+            httpOnly: true,
+            secure: true,
+            maxAge: process.env.COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000, // 1 day
+        };
+        res.cookie("token", token, cookieOptions);
+        res.status(200).json({ message: "USer logged in successfully" });
+    }
+    catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: "there is something error in logging in user", err })
+    }
+};
+
+const getUser = (req, res) => {
+    try {
+        res.status(200).json({
+            user: req.user,
+            success: true,
+            message: "User fetched successfully",
+        });
+    } catch (err) { }
+};
+
+const logoutUser = (req, res) => {
+    try {
+        res.clearCookie("tooken", {
+            httpOnly: true,
+            secure: true,
+        });
+        return res.status(200).json({ message: "user logged out succesfully" });
+    } catch (err) {
+        console.log(err);
+        return res.status(400).json({ message: "there us something wrong in logging out", err })
+    }
+};
+
+const forgotPassword = async (req, res) => {
+    const { email } = req.body;
+    if (!email) {
+        return res.status(400).json({ message: "please fill the email field" })
+    };
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ message: "user not found" })
+        }
+        const resetToken = crypto.randomBytes(332).toString("hex")
+
+        user.passwordResetToken = resetToken;
+        user.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+        await user.save();
+
+        const transport = nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USER,
+                pass: process.env.EMAIL_PASS
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_FROM,
+            to: user.email,
+            subject: "forgot password link ",
+            text: "your forgotpassword link will be expire in 10 min so please click on the link ",
+            html: `<b>Click Here</b>
+            <br/>
+            ${process.env.BASE_URL}/api/v1/users/reset-password/${resetToken}`,
+        };
+        await transport.sendMail(mailOptions);
+        res.status(200).json({ message: "reset password link sent to your email successfully" });
+    }
+    catch (err) {
+        res.status(400).json({ message: "there is some server error problem ", err });
+    };
+};
+
+const resetPassword = async (req, res) => {
+    const { token } = req.params
+    const { password, rePassword } = req.body
+    if (!password || !rePassword) {
+        return res.status(400).json({ message: "both field required" });
+
+        if (!token) {
+            return res.status(400).json({ message: "Invalid token" });
+        }
+        try {
+            const user = await User.findOne({
+                passwordResetToken: token,
+                passwordResetExpires: { $gt: Date.now() },
+            });
+            if (!user) {
+                return res.status(400).json({ message: "User not found" });
+            }
+            if (password !== rePassword) {
+                return res.status(400).json({ message: "password do not match" });
+            }
+            user.password = password;
+            user.passwordResetToken = undefined;
+            user.passwordResetExpires = undefined;
+
+            await user.save();
+
+            return res.status(200).json({ message: "Password reset successfully" });
+        } catch (err) {
+            return res.status(400).json({ message: "server Error", err });
+        };
+    };
+};
+
+export { register, login, verifyUser, getUser, logoutUser, forgotPassword, resetPassword }; 
