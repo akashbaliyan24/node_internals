46e547f6efb85b18eefe263b48107337e97c0f64
Akash <akashbaliya24@gmail.com>
2025-04-19 18:01:36 +0530

    create a file readme create routes

---
+++ readme.md
@@ -0,0 +1,15 @@
+# this is the authentication user backend for simple learning  
+
+
+# In this I learnt about 
+
+1. middleware 
+2. How to create Routes
+3. How to use node mailer 
+4. How to handle error 
+5. How to connect db 
+6. Use of .env file
+7. How to  create servers 
+8. Use of cors 
+
+
---
+++ routes/user.routes.js
@@ -0,0 +1,15 @@
+import express from "express"
+import authMiddleware from "../middleware/user.middleware"
+const Router = express.Router();
+
+import {register, login, verifyUser, getUser, logoutUser, forgotPassword, resetPassword} from "../controllers/user.controller.js"
+
+Router.post("/register",register);
+Router.get("/verify/:token",verifyUser);
+Router.post("/login",login);
+Router.get("/profile",authMiddleware,getUser);
+Router.post("/forgot-password",forgotPassword);
+Router.post("/reset-password/:token",resetPassword);
+Router.get("/logout",logoutUser)
+
+export default Router;
